17e126c6553c9210ba8c497776922c5de864f031
Roman Arefev <ra@example.com>
2021-04-03 23:46:01 +0300

    minoe fixes

--- hw01-gradle/src/main/java/com/atumra/HelloOtus.java
+++ hw01-gradle/src/main/java/com/atumra/HelloOtus.java
@@ -13,7 +13,8 @@ import com.google.common.base.Splitter;
 public class HelloOtus {
 
     public static void main(String[] args) {
-      Iterable<String> result = Splitter.on(',').trimResults().omitEmptyStrings().split("foo,bar,,   qux");
+      Iterable<String> result = Splitter.on(',')
+        .trimResults().omitEmptyStrings().split("foo,bar,,   qux");
       System.out.println(result);
     }
 }
