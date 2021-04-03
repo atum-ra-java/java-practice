b03796f6bceabb3fe7f42fd11e4bb2233622348a
Roman Arefev <ra@example.com>
2021-04-03 23:43:05 +0300

    guava method

---
+++ hw01-gradle/src/main/java/com/atumra/HelloOtus.java
@@ -0,0 +1,19 @@
+package com.atumra;
+
+import com.google.common.base.Splitter;
+
+/**
+ *
+ * To start the application:
+ * gradlew.bat build
+ * java -jar ./hw01-gradle/build/libs/gradleHelloWorld-0.1.jar
+ *
+ */
+
+public class HelloOtus {
+
+    public static void main(String[] args) {
+      Iterable<String> result = Splitter.on(',').trimResults().omitEmptyStrings().split("foo,bar,,   qux");
+      System.out.println(result);
+    }
+}
