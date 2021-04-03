a8ef3ec90b1e8bae45df7224fa16bfee2b35b769
Roman Arefev <ra@example.com>
2021-04-03 23:51:26 +0300

    final clean up

--- hw01-gradle/src/main/java/com/atumra/HelloOtus.java
+++ hw01-gradle/src/main/java/com/atumra/HelloOtus.java
@@ -5,15 +5,15 @@ import com.google.common.base.Splitter;
 /**
  *
  * To start the application:
- * gradlew.bat build
+ * ./gradlew
  * java -jar ./hw01-gradle/build/libs/gradleHelloWorld-0.1.jar
  *
  */
 
 public class HelloOtus {
 
-    public static void main(String[] args) {
-      Iterable<String> result = Splitter.on(',')
+    public static void main(final String[] args) {
+      final Iterable<String> result = Splitter.on(',')
         .trimResults().omitEmptyStrings().split("foo,bar,,   qux");
       System.out.println(result);
     }
