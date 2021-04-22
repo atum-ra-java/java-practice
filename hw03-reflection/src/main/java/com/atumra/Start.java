4d6b1cf449999ce781eb550a7392a2e5a2583b06
Roman Arefev <ra@example.com>
2021-04-22 19:21:02 +0300

    main class

---
+++ hw03-reflection/src/main/java/com/atumra/Start.java
@@ -0,0 +1,14 @@
+package com.atumra;
+
+import static com.atumra.Framework.*;
+
+public class Start {
+
+    public static void main(String[] args) {
+
+        System.out.println("\nRun tests for class TestInt...\n");
+        launchTest(TestInt.class);
+
+    }
+
+}
